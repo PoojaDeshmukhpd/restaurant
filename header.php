9278069dd09390e964ddc2f6ed959309adbadcdb
PoojaDeshmukhpd <101447967+PoojaDeshmukhpd@users.noreply.github.com>
2022-10-22 14:00:18 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,40 @@
+<header>
+	<nav class="navbar navbar-expand-lg navigation-wrap">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="images/New1.png" style="height: 9.6345rem; margin-top: 0%;width: 20.6345rem;" ></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+     <i class="fas fa-stream navbar-toggler-icon"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#explore-food">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#explore-food">Explore Foods</a>
+        </li>
+        
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#explore-food">Book Table</a>
+      </li>
+      
+        <li class="nav-item">
+          <a class="nav-link" href="#testimonial">Review</a>
+        </li>
+        </li>
+               <li class="nav-item">
+          <a class="nav-link" href="#faq">FAQ</a>
+        </li>
+       <li><button class="main-btn">1200 345 123</button></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
